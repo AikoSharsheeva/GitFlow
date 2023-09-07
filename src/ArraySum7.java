849c9fa9d8e7b9b2fc235df5352104244d8f02b1
AikoSharsheeva <calipsolee@gmail.com>
2023-09-07 20:49:31 +0600

    Array 7 using loop int i:array

---
+++ src/ArraySum7.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArraySum7 {
+    public static void main(String[] args) {
+        int[] array={5,1,9,3,7,8,4,6};
+        int sum=0;
+        for(int i: array ){
+            sum+=i;
+            System.out.println("The sum = " + sum);
+        }
+    }
+}
