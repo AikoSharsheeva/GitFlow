62c43e05ea12d217597995df09b853fc98635f9a
AikoSharsheeva <calipsolee@gmail.com>
2023-09-15 21:09:31 +0600

    just practice previuos lessons

---
+++ src/MapPractice.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapPractice {
+    public static void main(String[] args) {
+        String str = "hello world hello wedevx wedevx wedevx java";
+       String[] words=str.split(" ");
+       Map<String, Integer> stringIntegerMap=new HashMap<>();
+       for(String var:words){
+           Integer currentnum=0;
+           if(stringIntegerMap.containsKey(var)){
+               currentnum=stringIntegerMap.get(var);
+           }
+           stringIntegerMap.put(var, currentnum+1);
+       }
+        System.out.println(stringIntegerMap);
+    }
+}
